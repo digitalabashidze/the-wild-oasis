284a65673ac5bf232eb22ac3ec6a698f5d684c5f
digitalabashidze <digitalabashidze@gmail.com>
2024-05-02 18:05:30 +0400

    Update BookingDetail.jsx

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-
 import BookingDataBox from './BookingDataBox'
 import Row from '../../ui/Row'
 import Heading from '../../ui/Heading'
@@ -7,21 +6,15 @@ import Tag from '../../ui/Tag'
 import ButtonGroup from '../../ui/ButtonGroup'
 import Button from '../../ui/Button'
 import ButtonText from '../../ui/ButtonText'
-
-import { useMoveBack } from '../../hooks/useMoveBack'
-import { useBooking } from './useBooking'
 import Spinner from '../../ui/Spinner'
-import {
-	HiArrowDownOnSquare,
-	HiArrowUpOnSquare,
-	HiTrash,
-} from 'react-icons/hi2'
-import { useNavigate } from 'react-router-dom'
-import { useCheckout } from '../check-in-out/useCheckOut'
 import Modal from '../../ui/Modal'
 import ConfirmDelete from '../../ui/ConfirmDelete'
-import { useDeleteBooking } from './useDeleteBooking'
 import Empty from '../../ui/Empty'
+import { useMoveBack } from '../../hooks/useMoveBack'
+import { useBooking } from './useBooking'
+import { useNavigate } from 'react-router-dom'
+import { useCheckout } from '../check-in-out/useCheckout'
+import { useDeleteBooking } from './useDeleteBooking'
 
 const HeadingGroup = styled.div`
 	display: flex;
